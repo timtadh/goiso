5765113ed8eb66ca63e52c35208f55957f427e4f
Tim Henderson <tim.tadh@gmail.com>
2015-01-13 12:57:39 -0500

    minor whitespace fix
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- bliss/bliss.go
+++ bliss/bliss.go
@@ -3,20 +3,20 @@ package bliss
 /*
   Copyright (c) 2014 Tim Henderson
   Released under the GNU General Public License version 3.
-  
+
   This file is part of goiso a wrapper around bliss.
-  
+
   bliss is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License version 3
   as published by the Free Software Foundation.
-  
+
   bliss is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
-  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+  along with goiso.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /*
--- bliss/bliss_test.go
+++ bliss/bliss_test.go
@@ -2,18 +2,18 @@ package bliss
 /*
   Copyright (c) 2014 Tim Henderson
   Released under the GNU General Public License version 3.
-  
+
   This file is part of goiso a wrapper around bliss.
-  
+
   bliss is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License version 3
   as published by the Free Software Foundation.
-  
+
   bliss is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with Bliss.  If not, see <http://www.gnu.org/licenses/>.
 */
